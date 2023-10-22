9b037a6e4ba67ad8332c6dc52be50156c8f8d333
Travis Riegler <travisriegler@protonmail.com>
2023-10-21 20:19:37 -0400

    secrets manager and docker

--- src/main/java/com/qosocial/qosocialapi/controller/TestController.java
+++ src/main/java/com/qosocial/qosocialapi/controller/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 
     @GetMapping
     public ResponseEntity<String> getTest() {
-        return ResponseEntity.ok("CI/CD workflow test complete dfsdfsdfsdfsdf");
+        return ResponseEntity.ok("testing secrets manager and docker");
     }
 }
