a8c44362f0e74467c5a8604b215529e30a487859
Travis Riegler <travisriegler@protonmail.com>
2023-10-21 21:14:54 -0400

    secrets manager and docker6

--- src/main/java/com/qosocial/qosocialapi/controller/TestController.java
+++ src/main/java/com/qosocial/qosocialapi/controller/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 
     @GetMapping
     public ResponseEntity<String> getTest() {
-        return ResponseEntity.ok("charlie");
+        return ResponseEntity.ok("delta");
     }
 }
